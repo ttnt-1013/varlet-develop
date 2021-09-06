f238b7bf500f775c142a926eddaeec3dae815c67
ttnt-1013 <ttnt.1013@gmail.com>
2021-09-06 15:00:27 +0900

    First commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+import App from './App.vue'
+import Varlet from '@varlet/ui'
+import { createApp } from 'vue'
+import '@varlet/ui/es/style.js'
+
+createApp(App).use(Varlet).mount('#app')
